568794abd4d8751df4f202a1b9e51c814c0d67ed
suimiAA0011 <sm.seemo25@gmail.com>
2022-12-06 10:49:33 +0300

    Initial Commit

---
+++ Grass/GrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrassApp.swift
+//  Grass
+//
+//  Created by Salma on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
